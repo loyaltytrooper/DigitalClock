166d48a7ee27980634c2bb62e6366502f7e51fdd
Loyalty Trooper <loyaltytrooper@gmail.com>
2022-02-03 08:03:04 +0530

    10% Progress on stopwatch

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
+#include<conio.h>
+#include<ctime>
 using namespace std;
 
+void alarm();
+void digitalClock();
+void timer();
+void stopwatch();
 
 void menu()
 {
@@ -35,24 +41,54 @@ void digitalClock() // Name of Engineer developing this feature
 {
     // Please start writing your code here and please write comments so that others can understand what has been done
     // Feel free to make your own functions to support the functionality of your code
+    return;
 }
 
 void alarm() // Name of Engineer developing this feature
 {
     // Please start writing your code here and please write comments so that others can understand what has been done
     // Feel free to make your own functions to support the functionality of your code
+    return;
 }
 
 void timer() // Name of Engineer developing this feature
 {
     // Please start writing your code here and please write comments so that others can understand what has been done
     // Feel free to make your own functions to support the functionality of your code
+    return;
 }
 
 void stopwatch() // Name of Engineer developing this feature
 {
-    // Please start writing your code here and please write comments so that others can understand what has been done 
+    // Please start writing your code here and please write comments so that others can understand 
+    //what has been done 
     // Feel free to make your own functions to support the functionality of your code
+
+    cout << "Press:- " << endl
+         << "\t\t's' - Start the StopWatch" << endl
+         << "\t\t'l' for lap" << endl
+         << "\t\t'r' for reset" << endl;
+
+    int lapCount = 1;
+    
+
+    char button = getch();
+    while(button) // #include<conio.h>
+    {
+        if(button == 's')
+        {
+        }
+        else if(button == 'l')
+        {
+            cout << "Lap " << lapCount << ": " <<  << endl;
+            lapCount++;
+        }
+
+        else if(button == 'r')
+            break;
+        
+        button = getch();
+    }
 }
 
 int main()
